3749aa7a29078079e44893d817a0b58a2a64dd12
Nikunj Baheti <nikunj.baheti@gmail.com>
2024-07-21 16:33:37 +0530

    Update Historical Returns.py

--- Historical Returns.py
+++ Historical Returns.py
@@ -38,7 +38,6 @@ def fetch_json_and_save_csv():
 
             except requests.exceptions.RequestException as e:
                 print(f"Error fetching data for MFCode {mf_code}: {e}")
-
                 
 if __name__ == "__main__":
     fetch_json_and_save_csv()
